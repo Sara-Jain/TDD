e9f69a2efa017cf4fc1b7f4f91119d9e4667bb08
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-11 16:22:42 +0530

    feat: create test files for calculator.js

---
+++ calculator.js
@@ -0,0 +1,5 @@
+function add(number1 , number2 = 0){
+    return number1+number2;
+}
+
+module.exports = add;
---
+++ tests/calculator.test.js
@@ -0,0 +1,28 @@
+// add function
+
+// success case
+//  should give the sum of 2 numbers
+//  should return the input directly only 1 number is given
+//  should return the sum of first 2 numbers when more than 2 inputs are given
+
+// failure
+//  should throw invalid input when array given as input
+//  should throw invalid input when string given as input
+//  should throw error when integer raange exceeded
+
+const add = require('../calculator');
+
+describe('add function', function () {
+    it('should give the sum of 2 numbers', function () {
+        const sum = add(2, 3);
+        expect(sum).toBe(5);
+    });
+    it('should return the input directly only 1 number is given', function () {
+        const sum = add(2);
+        expect(sum).toBe(2);
+    });
+    it('should return the sum of first 2 numbers when more than 2 inputs are given', function () {
+        const sum = add(2, 3, 4, 6);
+        expect(sum).toBe(5);
+    });
+});
