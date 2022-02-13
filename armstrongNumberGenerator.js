d8cbdba1e6e0eeba7f1cad400342b29deb979664
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-13 10:26:01 +0530

    feat: success case for numberOfDigit

---
+++ armstrongNumberGenerator.js
@@ -0,0 +1,50 @@
+function numberOfDigit(number) {
+    let digitCount = 0;
+    // if (number === undefined) {
+    //     return "Enter the value"
+    // }
+    // if (typeof (number) !== "number" || number % 1 != 0) {
+    //     return "Invalid DataType"
+    // }
+    while (number != 0) {
+        digitCount++;
+        number = parseInt(number / 10, 10);
+    }
+    return digitCount;
+}
+
+function isArmstrongNumber(number) {
+    // if (typeof (number) !== 'number') {
+    //     return "Invalid datatype"
+    // }
+    // if (number < 0)
+    //     return "Negative value not allowed";
+    // if (number > 32767 || number < -32768 || number > 32767 || number < -32768) {
+    //     return "Inputs are out of range"
+    // }
+    const digitCount = numberOfDigit(number);
+    let temp = number, sum = 0;
+    while (temp != 0) {
+        let lastDigit = temp % 10;
+        sum += Math.pow(lastDigit, digitCount);
+        temp = parseInt(temp / 10, 10);
+    }
+    return sum === number;
+}
+
+function ArmstrongGenerator(range) {
+    // array to store the armstrong number in the given range
+    let armstrongNumbers = [];
+    for (let index = 0; index <= range; index++) {
+        if (isArmstrongNumber(index)) {
+            armstrongNumbers.push(index);
+        }
+    }
+    return armstrongNumbers;
+}
+
+module.exports = {
+    numberOfDigit: numberOfDigit,
+    isArmstrongNumber: isArmstrongNumber,
+    ArmstrongGenerator: ArmstrongGenerator
+}
